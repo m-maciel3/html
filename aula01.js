0f181ef0f2a383b81946e9c784324896446781f6
m-maciel3 <113068841+m-maciel3@users.noreply.github.com>
2022-09-15 23:45:16 -0400

    exercícios html, css, js

---
+++ aula01.js
@@ -0,0 +1,11 @@
+// alert("Olá navegador")
+console.log("Também estou aqui")
+
+function msgaluno() {
+    alert("Clicou em mim !!!")
+    console.log("Registrado o clique com sucesso.")
+}
+
+function mouseEvento() {
+    console.log("Registro copiado e enviado.")
+}
